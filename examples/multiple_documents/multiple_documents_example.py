69237498f1bb0445004400b99061b46025a1cfe1
Pedro Szekely <szekely1401@gmail.com>
2018-03-14 11:57:14 -0700

    refactor

--- examples/multiple_documents/multiple_documents_example.py
+++ examples/multiple_documents/multiple_documents_example.py
@@ -46,26 +46,25 @@ doc = etk.create_document(sample_input)
 new_doc = etk.create_document({})
 
 # put some data in the new document
-new_doc.store_extractions(doc.select_segments("displaced"), "size")
-new_doc.store_extractions(doc.select_segments("country"), "nationality")
-new_doc_dataset = doc.invoke_extractor(template_based_constructor,
-                                       doc.select_segments("dataset"),
-                                       template="{}/victim")
-new_doc.store_extractions(new_doc_dataset, "dataset")
+doc.store(doc.select("displaced"), "size")
+doc.store(doc.select("country"), "nationality")
+dataset = doc.apply(template_based_constructor, doc.select("dataset"), template="{}/victim")
+new_doc.store(dataset, "dataset")
 
 # set the doc_id of the new document.
 # note: if using hash, must call when content is ready; greater control could be
 # exercised by retrieving the JSON of the new doc, and calculating a custom doc_id.
 # must do something if the user forgets to define a doc_id
-new_doc.set_doc_id(new_doc.get_hash())
+new_doc.doc_id = etk.hash(new_doc.value)
+
+
+print(json.dumps(new_doc, indent=2))
 
 # in the new document, in_event points back to the parent document.
-# if the doc_id of the new document is not set yet, this would error out.
-new_doc.store_extractions(doc, "in_event")
+new_doc.store(doc.select("doc_id"), "in_event")
 
 # add a the new document as a value to the victim field.
-# again, if the doc_id of the new document is not set yet, this would error out.
-doc.store_extractions(new_doc, "victim")
+doc.store(new_doc.select("doc_id"), "victim")
 
 # tell ETK that we want to process this new docuemtn and add it to the KG
 etk.process_document(new_doc)
